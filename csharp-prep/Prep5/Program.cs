50db275ccd831b077295ec2295874c00dce65fac
mislangc <carlbpmislang@gmail.com>
2023-01-05 19:54:50 -0800

    C# Prep 5 Assignment: Finished

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -5,5 +5,38 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep5 World!");
+        Console.WriteLine("");
+
+        static void DisplayWelcome()
+        {
+            Console.WriteLine("Welcome to the Program!");
+        }
+        static string PromptUserName()
+        {
+            Console.Write("Please enter your name: ");
+            string name = Console.ReadLine();
+            return name;
+        }
+        static int PrompUserNumber()
+        {
+            Console.Write("Please enter your favorite number: ");
+            string input = Console.ReadLine();
+            int number = int.Parse(input);
+            return number;
+        }
+        static int SquareNumber(int num)
+        {
+            int squared = num * num;
+            return squared;
+        }
+        static void DisplayResult(string user, int number)
+        {
+            Console.WriteLine($"{user}, the square of your number is {number}");
+        }
+        DisplayWelcome();
+        string usersName = PromptUserName();
+        int favoriteNumber = PrompUserNumber();
+        int squaredFavoriteNumber = SquareNumber(favoriteNumber);
+        DisplayResult(usersName, squaredFavoriteNumber);
     }
 }
